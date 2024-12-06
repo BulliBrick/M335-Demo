2cbd4b171f6c6e1e7139c1ebc2806fca47d608b0
Simon Amsler <simon.amsler453@gmail.com>
2024-12-06 08:29:03 +0100

    initial commit

---
+++ android/app/src/main/java/m335/demo/MainActivity.java
@@ -0,0 +1,5 @@
+package m335.demo;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'm335.demo',
+  appName: 'm335-demo',
+  webDir: 'www'
+};
+
+export default config;
